f4c1b71c621d7feba5784047f73a06e84f899bba
Karan Gandhi <karandpr1@gmail.com>
2019-05-20 00:20:36 +0530

    Setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
+import './App.css';
+import {
+  IonApp, 
+  IonPage,
+  IonRouterOutlet,
+  IonSplitPane
+} from '@ionic/react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginPage from './pages/Login';
+ import ProfilePage from './pages/ProfilePage';
+ import SideMenu from './components/SideMenu';
+import HomePage from './pages/HomePage';
+ import ArticlePage from './pages/ArticlePage';
+import SettingsPage from './pages/Settings';
+ import NewArticlePage from './pages/NewArticle';
+
+class App extends Component {
+  constructor(props: any){
+        super(props);
+        }
+  render() {
+    return (
+      <Router>
+        <div className="App">        
+          <IonApp>
+          <IonSplitPane contentId="main">         
+            <SideMenu></SideMenu>
+            <IonPage id="main">        
+              <IonRouterOutlet>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/article/:slug" component={ArticlePage} />
+                <Route path="/profile/:authorname" component={ProfilePage} />
+                <Route exact path="/login" component={LoginPage} />                        
+                <Route path="/settings" component={SettingsPage} />
+                <Route path="/newarticle" component={NewArticlePage} />               
+              </IonRouterOutlet>
+            </IonPage>
+            </IonSplitPane>
+          </IonApp>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
